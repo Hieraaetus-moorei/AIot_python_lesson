7eb4cb85f7cb8b22ccafa1c5492852ed9ba91931
guest <b07b01081@ntu.edu.tw>
2024-10-23 14:57:05 +0800

    AIot python class finished

--- Module9/9-1.py
+++ Module9/9-1.py
@@ -1,8 +1,8 @@
 # import 自己建立的模組
+import myModule
 
-
-# 計算幾次方
-
+# 計算幾次方 by 取用 imported module
+print(myModule.pow(2, 3))
 
 # 簡單斷句
 
--- Module9/myModule.py
+++ Module9/myModule.py
@@ -1,9 +1,15 @@
 # 計算幾次方
-
+def pow (x, y):
+    return x**y
 
 
 # 簡單斷句
 
+# 是主程式才執行
+if __name__ == '__main__':
+    print('name is main')
+elif __name__ != '__main__':
+    print('This is another place')
 
 
 
